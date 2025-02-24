973a18a4ff313204bf0c0788dbe63af9c03ebe17
hyeonda02 <alceste951005@naver.com>
2025-02-24 21:38:58 +0900

    ♻️ Refactor : 내커리어 조회시, response 키 값 "대회"를 "공모전대회"로 수정

--- src/main/java/umc/kkijuk/server/career/service/CareerSearchServiceImpl.java
+++ src/main/java/umc/kkijuk/server/career/service/CareerSearchServiceImpl.java
@@ -94,7 +94,7 @@ public class CareerSearchServiceImpl implements CareerSearchService{
         Map<String, List<?>> careerList = new HashMap<>();
         careerList.put("대외활동",activities);
         careerList.put("동아리",circles);
-        careerList.put("대회",competitions);
+        careerList.put("공모전대회",competitions);
         careerList.put("교육",eduCareers);
         careerList.put("인턴",employments);
         careerList.put("프로젝트",projects);
