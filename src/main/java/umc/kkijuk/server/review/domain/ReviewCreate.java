c906d601a41f251f14379c8c27ffe4692d52dcf9
Suhun0331 <rlatngns6046@naver.com>
2025-03-06 10:39:44 +0900

    Refactor : 공고 후기 1000자 제한 추가 및 DB 컬럼 VARCHAR(1000)으로 변경

--- src/main/java/umc/kkijuk/server/review/domain/ReviewCreate.java
+++ src/main/java/umc/kkijuk/server/review/domain/ReviewCreate.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ public class ReviewCreate {
     private String title;
 
     @Schema(description = "공고 후기 내용", example = "Dijkstra 알고리즘을 활용하는 문제", type = "string")
+    @Size(max = 1000, message = "공고 후기 내용은 1000자 이내로 작성해주세요.")
     private String content;
 
     @NotNull(message = "날짜는 필수 입력 항목입니다.")
