34b5c061025b7c4510cafbafa3c1897f2efb4cbb
hyeonda02 <alceste951005@naver.com>
2025-03-09 00:00:36 +0900

    🐛 Fix : Optional.get() 제거 및 예외 처리 개선
    findById().get() 사용을 제거하고 orElseThrow()를 적용하여 NoSuchElementException 방지

--- src/main/java/umc/kkijuk/server/career/service/CareerSearchServiceImpl.java
+++ src/main/java/umc/kkijuk/server/career/service/CareerSearchServiceImpl.java
@@ -180,49 +180,56 @@ public class CareerSearchServiceImpl implements CareerSearchService{
     public BaseCareerResponse findCareer(Member requestMember, Long careerId, String type) {
         switch (type.toLowerCase()) {
             case "activity" -> {
-                Activity activity = activityRepository.findById(careerId).get();
+                Activity activity = activityRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 대외활동을 찾을 수 없습니다: " + careerId));
                 if(!activity.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
                 return getResponse(activity, ActivityResponse::new);
             }
             case "circle"  -> {
-                Circle circle = circleRepository.findById(careerId).get();
+                Circle circle = circleRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 동아리를 찾을 수 없습니다: " + careerId));
                 if(!circle.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
                 return getResponse(circle, CircleResponse::new);
             }
             case "project"  -> {
-                Project project = projectJpaRepository.findById(careerId).get();
+                Project project = projectJpaRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 프로젝트를 찾을 수 없습니다: " + careerId));
                 if(!project.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
                 return getResponse(project, ProjectResponse::new);
             }
             case "edu"  -> {
-                EduCareer eduCareer = eduCareerJpaRepository.findById(careerId).get();
+                EduCareer eduCareer = eduCareerJpaRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 교육을 찾을 수 없습니다: " + careerId));
                 if(!eduCareer.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
                 return getResponse(eduCareer, EduCareerResponse::new);
             }
             case "competition"  -> {
-                Competition competition = competitionJpaRepository.findById(careerId).get();
+                Competition competition = competitionJpaRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 공모전/대회를 찾을 수 없습니다: " + careerId));
                 if(!competition.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
                 return getResponse(competition, CompetitionResponse::new);
             }
             case "employment"  -> {
-                Employment employment = employmentJpaRepository.findById(careerId).get();
+                Employment employment = employmentJpaRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 경력을 찾을 수 없습니다: " + careerId));
                 if(!employment.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
                 return getResponse(employment, EmploymentResponse::new);
             }
             case "etc"  -> {
-                CareerEtc etc = etcRepository.findById(careerId).get();
+                CareerEtc etc = etcRepository.findById(careerId)
+                    .orElseThrow(() -> new NoSuchElementException("해당 기타활동을 찾을 수 없습니다: " + careerId));
                 if(!etc.getMemberId().equals(requestMember.getId())){
                     throw new OwnerMismatchException();
                 }
@@ -264,7 +271,8 @@ public class CareerSearchServiceImpl implements CareerSearchService{
 
     @Override
     public List<FindDetailResponse> findAllDetailByTag(Member requestMember, Long tagId, String sort) {
-        Tag tag = tagRepository.findById(tagId).get();
+        Tag tag = tagRepository.findById(tagId)
+            .orElseThrow(() -> new NoSuchElementException("해당 태그를 찾을 수 없습니다: " + tagId));
         if (!tag.getMemberId().equals(requestMember.getId())) {
             throw new OwnerMismatchException();
         }
@@ -427,37 +435,44 @@ public class CareerSearchServiceImpl implements CareerSearchService{
 
         switch (firstDetail.getCareerType()) {
             case ACTIVITY -> {
-                Activity activity = activityRepository.findById(firstDetail.getCareerId()).get();
+                Activity activity = activityRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 대외활동을 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = activity.getName();
                 alias = activity.getAlias();
             }
             case PROJECT -> {
-                Project project = projectJpaRepository.findById(firstDetail.getCareerId()).get();
+                Project project = projectJpaRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 프로젝트를 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = project.getName();
                 alias = project.getAlias();
             }
             case EMP -> {
-                Employment emp = employmentJpaRepository.findById(firstDetail.getCareerId()).get();
+                Employment emp = employmentJpaRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 경력을 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = emp.getName();
                 alias = emp.getAlias();
             }
             case EDU -> {
-                EduCareer edu = eduCareerJpaRepository.findById(firstDetail.getCareerId()).get();
+                EduCareer edu = eduCareerJpaRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 교육을 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = edu.getName();
                 alias = edu.getAlias();
             }
             case COM -> {
-                Competition competition = competitionJpaRepository.findById(firstDetail.getCareerId()).get();
+                Competition competition = competitionJpaRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 공모전/대회를 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = competition.getName();
                 alias = competition.getAlias();
             }
             case CIRCLE -> {
-                Circle circle = circleRepository.findById(firstDetail.getCareerId()).get();
+                Circle circle = circleRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 동아리를 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = circle.getName();
                 alias = circle.getAlias();
             }
             case ETC -> {
-                CareerEtc etc = etcRepository.findById(firstDetail.getCareerId()).get();
+                CareerEtc etc = etcRepository.findById(firstDetail.getCareerId())
+                    .orElseThrow(() -> new NoSuchElementException("해당 기타활동을 찾을 수 없습니다: " + firstDetail.getCareerId()));
                 title = etc.getName();
                 alias = etc.getAlias();
             }
