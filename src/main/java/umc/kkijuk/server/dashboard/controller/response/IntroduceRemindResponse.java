ed11f702693a52ed2f269b159612853ef810f1c8
Suhun0331 <rlatngns6046@naver.com>
2025-03-21 13:17:49 +0900

    Refactor : 홈 자기소개서 리마인더 중 공고 마감일자 지난 자기소개서 제외

--- src/main/java/umc/kkijuk/server/dashboard/controller/response/IntroduceRemindResponse.java
+++ src/main/java/umc/kkijuk/server/dashboard/controller/response/IntroduceRemindResponse.java
@@ -14,17 +14,23 @@ public class IntroduceRemindResponse {
     private Long introduceId;
     private String recruitTitle;
     private String deadline;
+    private Boolean recruitEndState;
 
     @Builder
     public IntroduceRemindResponse(Introduce introduce) {
         this.introduceId = introduce.getId();
         this.recruitTitle = introduce.getRecruit().toModel().getTitle();
         this.deadline = calculateTimeUntilDeadline(LocalDateTime.now(), introduce.getRecruit().toModel().getEndTime());
+        this.recruitEndState = isRecruitEnded(introduce.getRecruit().toModel().getEndTime());
     }
 
     private String calculateTimeUntilDeadline(LocalDateTime updatedAt, LocalDateTime deadline) {
         Duration duration = Duration.between(updatedAt, deadline);
         long days = duration.toDays() + 1;
         return "D-" + days;
+    }
+
+    private boolean isRecruitEnded(LocalDateTime deadline) {
+        return deadline.isBefore(LocalDateTime.now());
     }
 }
