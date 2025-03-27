93e9fc812a1e75da0eddc6c66e38d28223580b97
JeongHoon Lee <sherry0_@kakao.com>
2025-03-27 13:21:55 +0900

    Chore: monitoring via grafana, prometheus

--- src/main/java/umc/kkijuk/server/config/SecurityConfig.java
+++ src/main/java/umc/kkijuk/server/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/", "/swagger-ui/**", "/v3/api-docs/**", "/health-check/**",
-                                 "/oauth2/**",  "/auth/**", "/redis/check")
+                                 "/oauth2/**",  "/auth/**", "/redis/check", "/actuator/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
